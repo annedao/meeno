60c6c3c459f2a0f260a2ef5d551e0bf144a54892
larissahchiu <larissac@usc.edu>
2017-10-07 03:33:19 -0700

    WOOT WE ACTUALY DID SOMETHING

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
-var counter = 0;
-chrome.browserAction.onClicked.addListener(function(tab){
-  counter++;
-  if (counter == 5) {
-    alert("Hi");
-  }
+// background.js
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
 });
